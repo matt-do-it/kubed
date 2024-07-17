27d0a8b3ee0b71d237b0c726d9f278b6f5b2b25a
Matt Herold <mattherold@icloud.com>
2024-07-17 18:03:18 +0200

    Updated

--- Packer/shell/ubuntu-install.sh
+++ Packer/shell/ubuntu-install.sh
@@ -1,1 +1,27 @@
-#wget https://download.swift.org/swift-5.10.1-release/ubuntu2404/swift-5.10.1-RELEASE/swift-5.10.1-RELEASE-ubuntu24.04.tar.gz
+#!/bin/sh
+
+# apt-get install \
+#           binutils \
+#           git \
+#           gnupg2 \
+#           libc6-dev \
+#           libcurl4-openssl-dev \
+#           libedit2 \
+#           libgcc-13-dev \
+#           libncurses-dev \
+#           libpython3-dev \
+#           libsqlite3-0 \
+#           libstdc++-13-dev \
+#           libxml2-dev \
+#           libz3-dev \
+#           pkg-config \
+#           tzdata \
+#           unzip \
+#           zlib1g-dev
+# 
+# 
+# mkdir /opt/swift
+# cd /opt/swift
+# curl -LO https://download.swift.org/swift-5.10.1-release/ubuntu2404/swift-5.10.1-RELEASE/swift-5.10.1-RELEASE-ubuntu24.04.tar.gz
+# 
+# tar xzf 
--- ubuntu-amd64-boot.sh
+++ ubuntu-amd64-boot.sh
@@ -6,7 +6,7 @@ qemu-system-x86_64 \
   -accel tcg \
   -smp 4 \
   -m 4096 \
-  -drive file=Firmware/code-amd64.img,format=raw,if=pflash,readonly=on \
+  -drive file=Firmware/code-ubuntu-amd64.img,format=raw,if=pflash,readonly=on \
   -drive file=Packer/image_ubuntu_amd64/efivars.fd,format=raw,if=pflash \
   -device virtio-gpu-pci \
   -display default,show-cursor=on \
@@ -17,5 +17,6 @@ qemu-system-x86_64 \
   -device hda-duplex \
   -drive id=main,if=none,file=Packer/image_ubuntu_amd64/ubuntu.img,format=raw,cache=writethrough \
   -device virtio-blk-pci,drive=main \
-  -device virtio-net-pci,netdev=net0 -netdev socket,id=net0,fd=3 \
+  -device virtio-net-pci,mac=52:54:00:12:34:56,netdev=user.0 \
+  -netdev socket,id=user.0,fd=3 \
   -nographic
