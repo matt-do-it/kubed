5a48f6c7a98265bdcbf2c54b408c92a448216d4e
Matt Herold <mattherold@icloud.com>
2024-07-18 00:50:44 +0200

    Minicube

--- start-minikube.sh
+++ start-minikube.sh
@@ -1,5 +1,6 @@
-minikube start --driver=qemu \
-	--qemu-firmware-path=/opt/local/share/qemu/edk2-aarch64-code.fd \
+
+minikube start --v 8 --driver=qemu \
+	--qemu-firmware-path=/Users/herold/Virtual\ Machines/Firmware/code-aarch64.img \
 	--socket-vmnet-client-path=/opt/local/bin/socket_vmnet_client \
 	--socket-vmnet-path=/var/run/socket_vmnet \
 	--network socket_vmnet
